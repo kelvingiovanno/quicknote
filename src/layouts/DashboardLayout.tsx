54f64ba0fd317e71c1990958dcbccacb5786d2cb
Kelvin <kelvingiovanno2000@gmail.com>
2026-01-17 14:19:41 +0700

    add rounded on DashboardLayout.tsx

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 
 import Navbar from "@/components/layout/navbar";
-import SideBar from "@/components/layout/SideBar";
+import SideBar from "@/components/layout/sidebar";
 
 const DashboardLayout = () => {
     
@@ -10,7 +10,7 @@ const DashboardLayout = () => {
             <SideBar />
             <div className="ml-72 flex flex-col h-screen">
                 <Navbar />
-                <main className="bg-slate-100 flex-1">
+                <main className="bg-slate-100 flex-1 rounded-tl-xl">
                     <Outlet />
                 </main>
             </div>
