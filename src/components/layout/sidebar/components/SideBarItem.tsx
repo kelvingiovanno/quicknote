da843f7394de7f3fe0ffad6d3846cf2b99c58bcd
Kelvin <kelvingiovanno2000@gmail.com>
2026-01-20 15:44:26 +0700

    remove font-bold in active on sidebar

--- src/components/layout/sidebar/components/SideBarItem.tsx
+++ src/components/layout/sidebar/components/SideBarItem.tsx
@@ -18,7 +18,7 @@ const SideBarItem = (props : SideBarItemProps) => {
             className={`
                 w-full p-3 flex items-center gap-2 rounded-md hover:bg-black/10
                 transition-all duration-200 ease-in text-base
-                ${isActive ? "bg-black/10 font-bold" : "bg-white text-black/90" }
+                ${isActive ? "bg-black/10" : "bg-white text-black/90" }
             `}
         >
             {props.icon}
