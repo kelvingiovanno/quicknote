9773b928576a9fdf7c2f20ec1a7ee103cecff9a0
Kelvin <kelvingiovanno2000@gmail.com>
2026-01-20 15:51:10 +0700

    add Input component

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+    label?: string;
+    type: "text" | "password";
+}
+
+const Input = ({
+  label,
+  type,
+  ...rest  
+}: InputProps ) => {
+    return (
+        <div className="flex flex-col gap-1">
+            { label && <label htmlFor={type} className="text-sm text-black/70">{label}</label> }
+            <input type={type} {...rest} className="border-2 border-slate-200 text-base p-2 rounded-md outline-blue-500"/>
+        </div>
+    )
+}
+
+export default Input;
