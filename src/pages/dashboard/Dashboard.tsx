6cd6b21cd8c4b69cf7dfc941fdc86692e00e7ca4
Kelvin Giovanno <kelvingiovanno2000@gmail.com>
2026-01-26 14:24:21 +0700

    add DashboardLayout pages content

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router";
+import Button from "@/components/ui/Button";
+
 const Dashboard = () => {
 
     return (
         <>
+            <section className="mb-10">
+                <p className="text-2xl font-bold">Hello, Kelvin Giovanno</p>
+                <p className="text-based text-black/70">Here’s an overview of your notes and recent activity.</p>
+            </section>
+            {/* <section className="flex justify-start">
+                <Button title="Create Note" variant={"PRIMARY"}/>
+            </section> */}
+            <section className="flex flex-col gap-4">
+                <div className="flex justify-between items-center">
+                    <p className="text-xl font-bold">Recents Notes</p>
+                    <Link to={`/new-note`}>    
+                        <Button title={"Create Note"} variant={"PRIMARY"} />
+                    </Link>
+                </div>
+                <div className="grid xl:grid-cols-4 xl2:grid-cols-5 lg:grid-cols-3 grid-cols-2 gap-4">
+                    <div className="h-72 bg-white border border-slate-300 rounded-xl "></div>
+                    <div className="h-72 bg-white border border-slate-300 rounded-xl "></div>
+                    <div className="h-72 bg-white border border-slate-300 rounded-xl "></div>
+                    <div className="h-72 bg-white border border-slate-300 rounded-xl "></div>
+                    <div className="h-72 bg-white border border-slate-300 rounded-xl "></div>
+                    <div className="h-72 bg-white border border-slate-300 rounded-xl "></div>
+                    <div className="h-72 bg-white border border-slate-300 rounded-xl "></div>
+                    <div className="h-72 bg-white border border-slate-300 rounded-xl "></div>
+                </div>
+            </section>
         </>
     )
 }
