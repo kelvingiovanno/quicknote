7258af76dba865e9ad1f42f39c25940c7a07111a
Kelvin <kelvingiovanno2000@gmail.com>
2026-01-28 13:58:46 +0700

    remove document.body.classname for add bg color on authlayout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,8 +2,6 @@ import { Outlet } from "react-router";
 
 const AuthLayout = () => {
 
-    document.body.className = "bg-slate-200";
-
     return (
         <div className="min-w-3xl h-screen bg-slate-200 flex justify-center items-center">
             <main className="w-md bg-white p-8 rounded-lg border border-slate-300">
