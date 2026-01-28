a670830795bb88c684a4dce4e3583440f28d00f4
Kelvin <kelvingiovanno2000@gmail.com>
2026-01-28 14:00:47 +0700

    improve responsive on  dashboardlayout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -6,14 +6,14 @@ import SideBar from "@/components/layout/sidebar";
 const DashboardLayout = () => {
     
     return (
-        <div className="w-screen flex">
+        <div className="flex w-screen">
             <SideBar />
             <div className="flex flex-col h-screen flex-1">
                 <Navbar />
-                <main className="overflow-auto  h-10 bg-gray-200 flex-1 rounded-tl-xl border border-slate-300 p-8">
+                <main className="flex flex-col overflow-y-auto h-10 bg-gray-200 flex-1 md:rounded-tl-xl border border-slate-300  p-8 lg:p-16">
                     <Outlet />
                 </main>
-            </div>
+            </div>  
         </div>
     );
 }
