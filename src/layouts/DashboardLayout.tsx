31d8b09ffd2cd1f0539cd592c2f1f4a9cea965e6
Kelvin <kelvingiovanno2000@gmail.com>
2026-01-29 16:51:58 +0700

    refactor dashboardlayout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -2,19 +2,24 @@ import { Outlet } from "react-router";
 
 import Navbar from "@/components/layout/navbar";
 import SideBar from "@/components/layout/sidebar";
+import SidebarProvider from "@/providers/SidebarProvider";
 
 const DashboardLayout = () => {
     
     return (
-        <div className="flex w-screen">
-            <SideBar />
-            <div className="flex flex-col h-screen flex-1">
-                <Navbar />
-                <main className="flex flex-col overflow-y-auto h-10 bg-gray-200 flex-1 md:rounded-tl-xl border border-slate-300  p-8 lg:p-16">
-                    <Outlet />
-                </main>
-            </div>  
-        </div>
+        <SidebarProvider> 
+            <div className="flex w-screen">
+                <SideBar />
+                <div className="flex-1 flex flex-col h-screen">
+                    <Navbar />
+                    <main className="flex-1 overflow-y-auto bg-slate-200 md:rounded-tl-xl border border-slate-300">
+                        <div className="p-8 xl:mt-10 mx-auto max-w-7xl ">
+                            <Outlet />
+                        </div>
+                    </main>
+                </div>  
+            </div>
+        </SidebarProvider>
     );
 }
 
