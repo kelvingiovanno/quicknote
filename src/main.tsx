28fd85d6e0e7f19880cbc93d5ae7b3e265085021
Kelvin Giovanno <kelvingiovanno2000@gmail.com>
2026-02-06 00:48:54 +0700

    add AppProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from "react-router"
 
 import "@/styles/index.css"
 
-import router from "@/app/routes"
 import { AppProvider } from './app/provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppProvider>
-      <RouterProvider router={router} />
-    </AppProvider>
+    <AppProvider/>
   </StrictMode>,
 )
