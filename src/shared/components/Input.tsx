8e5b1e5574794bd3e45447246498d910db3a22bc
Kelvin <kelvingiovanno2000@gmail.com>
2026-02-06 16:18:24 +0700

    change color primary button to black

--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -13,7 +13,7 @@ const Input = ({
     return (
         <div className="flex flex-col gap-1">
             { label && <label htmlFor={type} className="text-sm text-black/70">{label}</label> }
-            <input type={type} {...rest} className="border-2 border-slate-200 text-base p-2 rounded-md outline-blue-500"/>
+            <input type={type} {...rest} className="border-2 border-slate-200 text-base p-2 rounded-md outline-none"/>
         </div>
     )
 }
