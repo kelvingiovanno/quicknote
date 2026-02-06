c467196e03169d50d81f476d1a22be51bffc9635
Kelvin <kelvingiovanno2000@gmail.com>
2026-02-06 16:20:07 +0700

    improve sidebar ux

--- src/app/layouts/components/sidebar/components/SideBarItem.tsx
+++ src/app/layouts/components/sidebar/components/SideBarItem.tsx
@@ -1,3 +1,4 @@
+import { useSidebar } from "@/app/provider";
 import { NavLink, useLocation } from "react-router";
 
 interface SideBarItemProps {
@@ -9,14 +10,15 @@ interface SideBarItemProps {
 const SideBarItem = (props : SideBarItemProps) => {
     
     const location = useLocation();
+    const sidebar = useSidebar();
 
     const isActive = location.pathname === props.navigateTo;
     
     return (
         <NavLink 
             to={props.navigateTo}
-            className={`
-                w-full p-3 flex items-center gap-2 rounded-md hover:bg-blue-50 transition-all duration-200 ease-in text-sm ${isActive ? "bg-blue-50 text-blue-500" : "bg-white text-black/80" }`}
+            className={`w-full p-3 flex items-center gap-2 rounded-md hover:bg-gray-200 transition-all duration-200 ease-in text-sm ${isActive ? "bg-gray-200 text-black" : "bg-white text-black/70" }`}
+            onClick={sidebar.closeSidebar}
         >
             {props.icon}
             {props.title}
